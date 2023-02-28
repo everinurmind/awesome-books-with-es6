a0f17a797d6b33b9110afa693e30c65909e13914
everinurmind <nurbol.sultanov@gmail.com>
2023-02-27 19:32:23 -0600

    add luxon date-time

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import BookApp from './modules/bookApp.js';
 import switchSections from './modules/switchSections.js';
+import { DateTime } from './modules/luxon.js';
 
 const bookApp = new BookApp();
 bookApp.displayBooks();
 
 switchSections();
+
+const dateTimeElement = document.getElementById('date-time');
+dateTimeElement.innerHTML = DateTime.now().toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
